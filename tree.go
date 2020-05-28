89b3b97042c49a818f133596aaa9acac4dc98941
henesy <henesy.dev@gmail.com>
2020-05-27 18:19:27 -0700

    glass existing parser

--- tree.go
+++ tree.go
@@ -61,95 +61,28 @@ func parse(ts *TokenScanner) (*Tree, error) {
 
 	switch token.Kind {
 	case Begin:
-		// TODO - validate the procedure token type, etc.
-		symtok := ts.next()
-
-		fmt.Println("» parsing operator =", symtok)
-
-		if symtok.Kind == NIL {
-			return nil, errors.New(fmt.Sprintf(`unexpected end of token stream at beginning of expression after token "%v"`, ts.previous().name))
-		}
-
-		symbol, err := token2symbol(symtok)
-		if err != nil {
-			return nil, err
-		}
-
-		tree, _ = NewTree(symbol)
-
-		// TODO - this hack works, does this mean the logic is flawed?
-		//ts.rewind()
-
-		// Recursive descent?
-		loop:
-		for {
-			token := ts.next()
-
-			fmt.Println("» next token =", token)
-
-			switch token.Kind {
-			case NIL:
-				// End of token stream
-				// TODO - just return?
-				break loop
-
-			case End:
-				// End of expression means nothing more to consume
-				// TODO - put at end of block?
-				return tree, nil
-
-			case Begin:
-				// Beginning of new expression
-				subtree, err := parse(ts)
-				if err != nil {
-					return nil, err
-				}
-
-
-				// TODO - more?
-				if subtree != nil {
-					tree.Children = append(tree.Children, subtree)
-				}
-
-				fmt.Println("»» Nested Begin length = ", tree.Length())
-			}
-
-
-			// Recursively descend on new expression
-			subtree, err := parse(ts)
-			if err != nil {
-				return nil, err
-			}
-
-			// TODO - more?
-			if subtree != nil {
-				tree.Children = append(tree.Children, subtree)
-			}
-
-			fmt.Println("»» Top Begin length = ", tree.Length())
-		}
+		// Beginning of an expression
+		token = ts.next()
 
 	case End:
-		// TODO - wrong?
-		return nil, nil
-		//return nil, errors.New(fmt.Sprintf(`"unexpected end of expression after "%v"`, ts.previous()))
+		// Closing of an expression
+		token = ts.next()
+		return tree, nil
+	}
+
+	symbol, err := token2symbol(token)
+	if err != nil {
+		return nil, errors.New(fmt.Sprint("token→symbol failed - ", err))
+	}
+
+	switch symbol.Kind {
+	case Procedure:
+		// Only thing not a leaf
+		;
 
 	default:
-		// TODO - use this rather than just pushing it through?
-		//return nil, errors.New(fmt.Sprintf(`could not parse for AST, unknown token type "%v"`, token.Kind))
-
-		// If we get something that isn't for expression control, return just that symbol as a mono-tree?
-		symbol, err := token2symbol(token)
-		if err != nil {
-			return nil, err
-		}
-
-		subtree, _ := NewTree(symbol)
-
-
-
-		return subtree, nil
-
+		// Leaf
+		return NewTree(symbol)
 	}
 
 	return tree, nil
